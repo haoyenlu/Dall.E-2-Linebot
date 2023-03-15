94665ab64f6d79a80ef687309fff7d2e91f95492
haoyenlu <haoenlu.07@gmail.com>
2023-03-15 01:02:42 -0400

    send multiple images using carousel

--- app.py
+++ app.py
@@ -99,14 +99,18 @@ def handle_message(event):
     else:
         images_url = generate_image(prompt = event.message.text,number=users_info[event.source.user_id]["number"])
 
-        image_messages = []
+        image_carousel_columns = []
         for i in range(len(images_url)):
-            image_messages.append(
-                ImageSendMessage(original_content_url=images_url[i],
-                                 preview_image_url=images_url[i])
+            image_carousel_columns.append(
+                ImageCarouselColumn(image_url=images_url[i],
+                                    action=URIAction(label="image url",uri=images_url[i]))
             )
 
-        line_bot_api.reply_message(event.reply_token,image_messages)
+        image_carousel_template = ImageCarouselTemplate(columns=image_carousel_columns)
+
+        line_bot_api.reply_message(event.reply_token,TemplateSendMessage(
+            alt_text="image carousel template",template=image_carousel_template))
+        
     return 'OK'
 
 
